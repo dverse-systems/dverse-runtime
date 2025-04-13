a4f9a653b2d334063ee98225924fa0689a9448e4
Sridhar Ananthakrishnan <sridhar@kriyaetive.com>
2025-04-13 23:41:04 +0530

    Initial commit - (POC with wasmi crate)

--- examples/wasm_add/target/wasm32-unknown-unknown/release/deps/wasm_add.d
+++
@@ -1,5 +0,0 @@
-/Users/sridhar/Projects/kriyo/kapsule-rt/examples/wasm_add/target/wasm32-unknown-unknown/release/deps/wasm_add.wasm: src/lib.rs
-
-/Users/sridhar/Projects/kriyo/kapsule-rt/examples/wasm_add/target/wasm32-unknown-unknown/release/deps/wasm_add.d: src/lib.rs
-
-src/lib.rs:
--- examples/wasm_add/target/wasm32-unknown-unknown/release/wasm_add.d
+++
@@ -1,1 +0,0 @@
-/Users/sridhar/Projects/kriyo/kapsule-rt/examples/wasm_add/target/wasm32-unknown-unknown/release/wasm_add.wasm: /Users/sridhar/Projects/kriyo/kapsule-rt/examples/wasm_add/src/lib.rs
